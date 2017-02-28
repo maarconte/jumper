c7d935c1cf745b6a4f8c2f4c576db201c9b10b87
Mathilde Arconte <mathilde@m-arconte.fr>
2017-02-28 22:45:07 +0100

    Debug Header-Image

--- template-parts/header/header-image.php
+++ template-parts/header/header-image.php
@@ -12,23 +12,21 @@
 <div class="custom-header">
 
 	<div class="custom-header-media">
-	<?php if ( is_home() ) { ?>
-		<?php the_custom_header_markup(); ?>
-	<?php } else if (is_archive() || is_single()) { 
-	if (is_archive()) {
-		$cat_id = $cat;
-	} else {
-		$categories = get_the_category();
-		$cat_id = $categories[0]->term_id;
-	}
-
-		?>
+ <?php if (is_category() || is_single()) { 
+					if (is_category()) {
+						$cat_id = $cat;
+					} else {
+						$categories = get_the_category();
+						$cat_id = $categories[0]->term_id;
+					}
+	?>
 		<img src="<?php the_field('image', 'category_'.$cat_id); ?>" alt="">
 	<?php } else if (is_page()) {
 		the_post_thumbnail();
+} else {
+	the_custom_header_markup();
 }
 		?>
-	<p> <?php echo $category ;?></p>
 	</div>
 	<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
 </div><!-- .custom-header -->
